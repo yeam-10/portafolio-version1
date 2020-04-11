e8fddbffea9ee39e5301523ad6cb5157a0019096
yeam-10 <anyeimarrequena@gmail.com>
2020-04-11 03:24:58 -0430

    portafolio version1

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let ubicacionPrincipal = window.pageYOffset; //0 corrida en frio
+
+AOS.init();
+
+window.addEventListener("scroll", function() {
+    let desplazamientoActual = window.pageYOffset; //100
+    if (ubicacionPrincipal >= desplazamientoActual) { //0 > 100
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    } else {
+
+        document.getElementsByTagName('nav')[0].style.top = "-100px"
+    }
+    ubicacionPrincipal = desplazamientoActual;
+
+})
+
+//Menu
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function() {
+
+    if (semaforo) {
+
+        document.querySelectorAll(".hamburguer")[0].style.color = "#fff";
+        semaforo = false;
+    } else {
+        document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+        semaforo = true
+    };
+
+    enlacesHeader.classList.toggle("menudos")
+})
